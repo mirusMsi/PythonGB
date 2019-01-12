8aff6b769772e7f49d4361a39bc56c09ac3bd1ae
MiroshkinSergey <miroshkin.mirus@ya.ru>
2019-01-12 15:30:14 +0300

    1. Определил задания как функции 2. Определил main function для запуска заданий по номеру 3. Встроил проверку на тип вводимых значений для определения задания 4. Встроил проверку на диапазон возможных значений номеров заданий

--- Python-projects/hw01_easy.py
+++ Python-projects/hw01_easy.py
@@ -7,15 +7,17 @@ __author__ = 'Сергей Иванович Мирошкин (miroshkin.mirus@ya
 # * постарайтесь решить задачу с применением арифметики и цикла while;
 # * при желании решите задачу с применением цикла for.
 
-number = str(input('Enter some integer:\n'))
-answer = []
-length = len(number)
 
-for i in range(length):
-    answer.append(int(number[i:(i + 1)]))
+def task_first():
+    number = str(input('Enter some integer:\n'))
+    answer = []
+    length = len(number)
 
-for i in range(length):
-    print(answer[i])
+    for i in range(length):
+        answer.append(int(number[i:(i + 1)]))
+
+    for i in range(length):
+        print(answer[i])
 
 
 # Задача-2: Исходные значения двух переменных запросить у пользователя.
@@ -26,7 +28,35 @@ for i in range(length):
 # Не нужно решать задачу так:
 # print("a = ", b, "b = ", a) - это неправильное решение!
 
+value_one = input('Enter some first value:\n')
+value_two = input('Enter some second value:\n')
+
+buffer = value_one
+value_one = value_two
+value_two = buffer
+
+print(value_one, "\n", value_two)
+
 
 # Задача-3: Запросите у пользователя его возраст.
 # Если ему есть 18 лет, выведите: "Доступ разрешен",
 # иначе "Извините, пользование данным ресурсом только с 18 лет"
+
+def main():
+    task = 0
+
+    while True:
+        task = input("Enter task's number which you want to check (integer from one to three or 'no' for exit): ")
+        if isinstance(task, int):
+            if 0 < task < 4:
+                break
+            else:
+                print('You entered incorrect number, please will try again.')
+        else:
+            print('You entered not integer, please will try again.')
+
+    if task == 1:
+        task_first()
+    elif task == 2:
+
+main()
